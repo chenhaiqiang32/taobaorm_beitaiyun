f0ae090621a43ce881408a1d0258e67612e1d106
仓木熏啊 <>
2025-06-08 22:23:26 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/taobaorm_beitaiyun/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: ["three"],
-        },
-      },
-    },
-  },
-});
+export default {
+  base: "./",
+};
